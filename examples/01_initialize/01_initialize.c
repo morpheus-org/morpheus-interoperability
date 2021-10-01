d0f4633a49fd9dc3e13919e26a185ffc51e98178
cstyl <cstyl16@gmail.com>
2021-10-01 16:32:27 +0100

    initial examples build

---
+++ examples/01_initialize/01_initialize.c
@@ -0,0 +1,53 @@
+/**
+ * 01_initialize.c
+ * 
+ * EPCC, The University of Edinburgh
+ * 
+ * (c) 2021 The University of Edinburgh
+ * 
+ * Contributing Authors:
+ * Christodoulos Stylianou (c.stylianou@ed.ac.uk)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// #include <Morpheus_Core.hpp>
+#include "Fcl_Util.h"
+
+int main(){
+
+    c_morpheus_initialize_without_args();
+
+    c_morpheus_finalize();
+
+    return 0;
+}
+
+// extern "C" {
+
+//   void c_axpy_view( view_type **v_y, view_type **v_x, double *alpha ) {
+//     using flcl::view_from_ndarray;
+
+//     view_type y = **v_y;
+//     view_type x = **v_x;
+
+//     Kokkos::parallel_for( "axpy", y.extent(0), KOKKOS_LAMBDA( const size_t idx)
+//     {
+//       y(idx) += *alpha * x(idx);
+//     });
+
+//     return;
+//   }
+
+
+// }
